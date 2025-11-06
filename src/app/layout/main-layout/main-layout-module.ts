a13f99681a6bbe240289ea9b0c649cff7993f504
Bruno Vitalino <brunovitalino@hotmail.com>
2025-11-06 02:01:59 -0300

    Adicionado componentes de header, sidebar e footer.

--- src/app/layout/main-layout/main-layout-module.ts
+++ src/app/layout/main-layout/main-layout-module.ts
@@ -3,10 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MainLayout } from './main-layout';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from '../../shared/shared-module';
 
 
 
@@ -18,10 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     RouterModule,
     MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonModule
+    SharedModule
   ]
 })
 export class MainLayoutModule { }
---
+++ src/app/shared/shared-module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Header } from './header/header';
+import { Sidebar } from './sidebar/sidebar';
+import { Footer } from './footer/footer';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+@NgModule({
+  declarations: [
+    Header,
+    Sidebar,
+    Footer
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
+  ],
+  exports: [
+    Header,
+    Sidebar,
+    Footer
+  ]
+})
+export class SharedModule { }
